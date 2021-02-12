b4b7ac064923eb2ac3a9860a7d25e217eac34c93
datikken <tikken23@gmail.com>
2021-02-12 22:02:53 +0300

    typescript bootstrap vue electron

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="info" class="bg-dark">
+      <b-navbar-brand href="#">Merchant book</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navbar extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
